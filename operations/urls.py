10279810a6876b11404c85346190417e0ec5a0b6
reactVishnu <thirdparties47@gmail.com>
2023-12-05 00:44:48 +0530

    CRUD Operations Added

--- operations/urls.py
+++ operations/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import UserDetailsListView, IndividualEmployeeDetailView
+from .views import UserDetailsListView, IndividualEmployeeDetailView, EmployeeCreateView, EmployeeUpdateView, EmployeeDeleteView
 
 app_name='operations'
 
 urlpatterns=[path('details', UserDetailsListView.as_view(), name='all details'),
-             path('detail/<str:employee_code>', IndividualEmployeeDetailView.as_view(), name='detail')
+             path('detail/<str:employee_code>', IndividualEmployeeDetailView.as_view(), name='detail'),
+             path('create', EmployeeCreateView.as_view(), name='create'),
+             path('update/<str:employee_code>',EmployeeUpdateView.as_view(), name='update'),
+             path('delete/<str:employee_code>',EmployeeDeleteView.as_view(), name='delete')
              ]
--- operations/views.py
+++ operations/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, UpdateView, CreateView, DeleteView
 from core.models import Details
 from django.shortcuts import get_object_or_404
+
 
 # Create your views here.
 
@@ -19,3 +21,34 @@ class IndividualEmployeeDetailView(DetailView):
     def get_object(self, queryset=None):
         # Use employee_code for lookup instead of primary key
         return get_object_or_404(Details, employee_code=self.kwargs['employee_code'])
+
+
+class EmployeeUpdateView(UpdateView):
+    model = Details
+    fields = '__all__'
+    template_name = "operations/create_emp.html"
+    success_url = reverse_lazy('operations:all details')
+
+    def get_object(self, queryset=None):
+        # Use employee_code for lookup instead of primary key
+        return get_object_or_404(Details, employee_code=self.kwargs['employee_code'])
+
+
+class EmployeeCreateView(CreateView):
+    model = Details
+    fields = '__all__'
+    template_name = 'operations/create_emp.html'
+    success_url = reverse_lazy('operations:all details')
+
+
+class EmployeeDeleteView(DeleteView):
+    model = Details
+    template_name = "operations/emp_delete.html"
+    context_object_name = 'employee'
+    success_url = reverse_lazy('operations:all details')
+
+    def get_object(self, queryset=None):
+        # Use employee_code for lookup instead of primary key
+        return get_object_or_404(Details, employee_code=self.kwargs['employee_code'])
+
+
