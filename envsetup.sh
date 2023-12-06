3296c250fb87d2648b2aab64b618cb5d1d54328b
reactVishnu <thirdparties47@gmail.com>
2023-12-07 02:36:27 +0530

    changed

--- envsetup.sh
+++ envsetup.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
-if [-d "env"]
-then
-  echo "Python virtual env exists"
-  else
-    sudo python -m venv env
-  fi
 
-echo $PWD
+if [ -d "env" ]
+then
+    echo "Python virtual environment exists."
+else
+    python3 -m venv env
+fi
+
 source env/bin/activate
+
 
 pip3 install -r requirements.txt
 
-if [-d "logs"]
+if [ -d "logs" ]
 then
-  echo "Log Folder exists"
+    echo "Log folder exists."
 else
-  mkdir logs
-  touch logs/error.log logs/access.log
+    mkdir logs
+    touch logs/error.log logs/access.log
 fi
-
-sudo chmod -R 777 logs
-echo "env setup finished"
